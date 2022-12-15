c4617c83dfe5bd5498384b04dce1f9787464073a
Kiko Seijo <kiko@sunnyface.com>
2022-12-15 17:28:20 +0100

    Initial style with tailwindcss.com

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="w-20 h-20 fill-current text-turquesa-500" />
             </a>
         </x-slot>
 
