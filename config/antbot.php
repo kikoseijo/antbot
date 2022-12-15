1de5b5b4a470c4b3415137c0c427f07b07a82395
Kiko Seijo <kiko@sunnyface.com>
2022-12-15 14:10:20 +0100

    Configure users.

---
+++ config/antbot.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'roles' => [
+        '1' => 'Admin',
+        '2' => 'Client',
+    ],
+];
---
+++ database/migrations/2022_12_15_122922_add_fields_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('admin')->default(0)->after('password');
+            $table->unsignedTinyInteger('role')->default(2)->after('admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['admin', 'role']);
+        });
+    }
+};
