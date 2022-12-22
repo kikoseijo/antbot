264a52e6726eeb0df76350c393624f0ddf8bbbff
Kiko Seijo <kiko@sunnyface.com>
2022-12-22 12:17:19 +0100

    Theme switcher.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    // darkMode: 'class',
+    darkMode: 'media',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
