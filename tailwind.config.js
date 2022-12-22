fefbbe286aa3d25f2f5a40d90b49c483fec7ffcd
Kiko Seijo <kiko@sunnyface.com>
2022-12-22 12:07:05 +0100

    Added flowbite js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
@@ -16,5 +17,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('flowbite/plugin')
+    ],
 };
