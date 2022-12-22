59be3b4f868ec2ab2a6ec274cc0d35e5fcf819b6
Kiko Seijo <kiko@sunnyface.com>
2022-12-22 19:22:29 +0100

    Code!

--- resources/js/code-mirror.js
+++ resources/js/code-mirror.js
@@ -10,10 +10,38 @@ import 'codemirror/mode/php/php.js';
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     lineNumbers: true,
     theme: 'dracula',
-    matchBrackets: true,
-    mode: "application/json",
     indentUnit: 4,
     indentWithTabs: true,
     tabSize: 4,
     lineWrapping: true,
+
+
+    mode: {name: "javascript", json: true},
+    extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
+    foldGutter: true,
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+    foldOptions: {
+      widget: (from, to) => {
+        var count = undefined;
+
+        // Get open / close token
+        var startToken = '{', endToken = '}';
+        var prevLine = window.editor_json.getLine(from.line);
+        if (prevLine.lastIndexOf('[') > prevLine.lastIndexOf('{')) {
+          startToken = '[', endToken = ']';
+        }
+
+        // Get json content
+        var internal = window.editor_json.getRange(from, to);
+        var toParse = startToken + internal + endToken;
+
+        // Get key count
+        try {
+          var parsed = JSON.parse(toParse);
+          count = Object.keys(parsed).length;
+        } catch(e) { }
+
+        return count ? `\u21A4${count}\u21A6` : '\u2194';
+      }
+    }
 });
