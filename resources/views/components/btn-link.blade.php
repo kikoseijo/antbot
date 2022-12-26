cc14d6a665d8433f571f5475b67716ec77cba5de
Kiko Seijo <kiko@sunnyface.com>
2022-12-26 06:55:04 +0100

    Build assets.

--- resources/views/components/btn-link.blade.php
+++ resources/views/components/btn-link.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-yellow-300 border border-transparent rounded-md font-medium text-xs text-white uppercase tracking-widest hover:bg-yellow-200 active:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-medium text-xs text-white uppercase tracking-widest hover:bg-yellow-300 active:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-800 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </a>
