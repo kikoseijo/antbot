9e34fb420f1914a265c94e868eb11df592b88abe
Kiko Seijo <kiko@sunnyface.com>
2023-01-13 05:40:29 +0100

    Protected system logs area.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // Custom made
+        'admin' => \App\Http\Middleware\IsAdmin::class,
     ];
 }
