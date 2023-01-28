1ea50bbd8ff3549a0cba8b2cf79f8782aa6d14b0
Kiko Seijo <kiko@sunnyface.com>
2023-01-29 00:06:35 +0100

    Fix for first user as admin.

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'admin',
+        'role',
         'timezone',
         'password',
     ];
