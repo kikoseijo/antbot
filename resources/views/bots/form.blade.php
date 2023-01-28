8e624910871a67595c1ca673fe7fb3ab592a8499
Kiko Seijo <kiko@sunnyface.com>
2023-01-28 22:37:53 +0100

    Added new mode title.

--- resources/views/bots/form.blade.php
+++ resources/views/bots/form.blade.php
@@ -128,7 +128,7 @@
                 <x-input-error class="mt-2" :messages="$errors->get('bot.show_logs')" />
             </div>
             <div>
-                <x-input-label for="oh_mode" :value="__('Write logs')" />
+                <x-input-label for="oh_mode" :value="__('Enable minute mark checks instead of continuously')" />
                 <div class="flex mt-2">
                     <div class="flex items-center mr-4">
                         <input id="oh_mode_1" type="radio" value="1"  wire:model="bot.oh_mode" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
